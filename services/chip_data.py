20bbace0f656832af8dc9eedc5ae66ecc5ec4c8d
daniel0203000 <d8289751@gmail.com>
2025-11-19 17:59:04 +0800

    Update chip_data.py
    放入評分code並修改評分參數

--- services/chip_data.py
+++ services/chip_data.py
@@ -194,70 +194,58 @@ def calculate_chip_indicators(stock_id: str):
     df['主力買賣超佔比'] = (df['主力買賣超'].abs() / df['Volume']).round(4)
 
     df['Score'] = 0
-
+    
     # 1. 單日買賣超
     df['Score'] += np.where(df['外資'] > 0, 1, -1)
     df['Score'] += np.where(df['投信'] > 0, 1, -1)
     df['Score'] += np.where(df['自營商'] > 0, 1, -1)
     df['Score'] += np.where(df['主力買賣超'] > 0, 1, -1)
-
+    
     # 2. 連續買賣天數
     def score_streak(x):
         if x >= 3: return 2
         elif x <= -3: return -2
         else: return 0
-
+    
     for c in ['外資連續買賣','投信連續買賣','自營連續買賣','主力連續買賣']:
         df['Score'] += df[c].apply(score_streak)
-
+    
     # 3. 持股佔比
-    # 外資佔比
     df['Score'] += np.where(df['外資佔比'] > 0.15, 2,
-                    np.where(df['外資佔比'] >= 0.05, 1, 0))
-
-    # 投信佔比
+                     np.where(df['外資佔比'] >= 0.05, 1, 0))
     df['Score'] += np.where(df['投信佔比'] > 0.1, 2,
-                    np.where(df['投信佔比'] >= 0.05, 1, 0))
-
-    # 自營商佔比
+                     np.where(df['投信佔比'] >= 0.05, 1, 0))
     df['Score'] += np.where(df['自營商佔比'] > 0.04, 1, 0)
-
-    # 主力買賣超佔比
     df['Score'] += np.where(df['主力買賣超佔比'] > 0.12, 2,
-                    np.where(df['主力買賣超佔比'] >= 0.07, 1, 0))
-
+                     np.where(df['主力買賣超佔比'] >= 0.07, 1, 0))
+    
     # 4. 融資券
-    # 融資變動比
-    df['融資變動比'] = df['融資增減'] / df['融資餘額']
-    df['融券變動比'] = df['融券增減'] / df['融券餘額']
+    df['融資變動比'] = (df['融資增減'] / df['融資餘額']).round(4)
+    df['融券變動比'] = (df['融券增減'] / df['融券餘額']).round(4)
     df['Score'] += np.where(df['融資變動比'] > 0.05, -1,
-                    np.where(df['融資變動比'] > 0.02, 2,
-                    np.where(df['融資變動比'] < -0.05,  -2,
-                    np.where(df['融資變動比'] < -0.02,  1, 0))))
+                     np.where(df['融資變動比'] > 0.02, 2,
+                     np.where(df['融資變動比'] < -0.05,  -2,
+                     np.where(df['融資變動比'] < -0.02,  1, 0))))
     df['Score'] += np.where(df['融券變動比'] > 0.05,  -2,
-                    np.where(df['融券變動比'] > 0.02,  1,
-                    np.where(df['融券變動比'] < -0.05, -1,
-                    np.where(df['融券變動比'] < -0.02, 2, 0))))
-
-    # 融資增減
-    df['Score'] += np.where(df['融資增減'] > 0, 1, 0)
-    df['Score'] += np.where(df['融資增減'] < 0, -1, 0)
-
-    # 融券增減
-    df['Score'] += np.where(df['融券增減'] > 0, -1, 0)
-    df['Score'] += np.where(df['融券增減'] < 0, 1, 0)
-
-    # 券資比
-    df['Score'] += np.where(df['融券券資比%'] > 0.2, 2,
-                    np.where(df['融券券資比%'] >= 0.1, 1, 0))
-
-    df['Score'] += np.where((df['融資增減'] > 0) & (df['融券增減'] < 0), 3, 0)
-    df['Score'] += np.where((df['融券增減'] > 0) & (df['融資增減'] < 0), -3, 0)
-
+                     np.where(df['融券變動比'] > 0.02,  1,
+                     np.where(df['融券變動比'] < -0.05, -1,
+                     np.where(df['融券變動比'] < -0.02, 2, 0))))
+    
+    df['Score'] += np.where(df['融資增減'] > 0, 1, -1)
+    df['Score'] += np.where(df['融券增減'] > 0, -1, 1)
+    
+    df['Score'] += np.where(df['融券券資比%'] < 12, 2,
+                     np.where(df['融券券資比%'] <= 20, 1, -2))
+    
+    df['Score'] += np.where((df['融資增減'] > 0) & (df['融券增減'] < 0), 2, 0)
+    df['Score'] += np.where((df['融券增減'] > 0) & (df['融資增減'] < 0), -2, 0)
+    
+    
     df['close_result'] = (df['Close']>df['Close'].shift(1)).astype(int)
     df['accurate'] = (((df['Score'] > 0) & (df['close_result'].shift(-1) == 1)) |((df['Score'] <= 0) & (df['close_result'].shift(-1) == 0))).astype(int)
     df = df.drop(columns=['Volume'])
     df = df.iloc[::-1]
+    df['評級'] = np.select([df['Score'] > 6,df['Score'] > 1,df['Score'] > -4],['很偏多', '偏多', '偏空'],default='很偏空')
     df['direction'] = np.where(df['Score'] <= 0, -1, 1)
     latestdata = df.iloc[0]
     latestdata_dict = latestdata.to_dict()
